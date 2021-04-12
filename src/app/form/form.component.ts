b576723069af603c57418d8dad12e5c356ac5856
z1178392022 <z1178392022@gmail.com>
2021-04-12 13:21:32 -0400

    1 file modified

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,44 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import {StorageService} from '../storage.service';
+import { Component, OnInit } from "@angular/core";
+import { StorageService } from "../storage.service";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.css"]
 })
 export class FormComponent implements OnInit {
+  peopleinfo: any = {
+    username: this.storage.get("username"),
+    sex: this.storage.get("sexy"),
+    citys: ["NewYork", "LosAngeles", "California"],
+    city: this.storage.get("city"),
+    hobby: [
+      {
+        title: "Basketball",
+        checked: this.storage.get("Basketball")
+      },
+      {
+        title: "VideoGame",
+        checked: this.storage.get("VideoGame")
+      },
+      {
+        title: "Coding",
+        checked: this.storage.get("Coding")
+      }
+    ],
+    lastwords: this.storage.get("lastwrods")
+  };
+  constructor(public storage: StorageService) {}
 
-  peopleinfo:any={
-    username: this.storage.get('username'),
-    sex: this.storage.get('sexy'),
-    citys:["NewYork","LosAngeles","California"],
-    city: this.storage.get('city'),
-    hobby:[{
-      title:'Basketball',
-      checked:false
-    },{
-      title:'VideoGame',
-      checked:false
-    },{
-      title:'Coding',
-      checked:false
-    }],
-    lastwords: this.storage.get('lastwrods')
+  ngOnInit(): void {}
+  dosubmit() {
+    this.storage.set("username", this.peopleinfo.username);
+    this.storage.set("sexy", this.peopleinfo.sex);
+    this.storage.set("city", this.peopleinfo.city);
+    this.storage.set("Basketball", this.peopleinfo.hobby[0].checked);
+    this.storage.set("VideoGame", this.peopleinfo.hobby[1].checked);
+    this.storage.set("Coding", this.peopleinfo.hobby[2].checked);
+    this.storage.set("lastwrods", this.peopleinfo.lastwords);
+    alert(
+      "Information saved in your Local Storage successfully, try reflash the page."
+    );
   }
-  constructor(public storage:StorageService) {
-    
-   }
-
-  ngOnInit(): void {
-  }
-  dosubmit(){
-    this.storage.set('username', this.peopleinfo.username);
-    this.storage.set('sexy', this.peopleinfo.sex);
-    this.storage.set('city', this.peopleinfo.city);
-    this.storage.set('hobby', this.peopleinfo.hobby.checked);
-    this.storage.set('lastwrods', this.peopleinfo.lastwords);
-    alert('Information saved in your Local Storage successfully, try reflash the page.')
-  }
-
-
 }
